6e0d6f7d9ecf95d7e9af8b5c127b12485ca672a9
Timo <timo.juez@gmail.com>
2020-03-21 16:34:55 +0100

    use denon.sh instead of telnet

--- include.sh
+++ include.sh
@@ -1,6 +1,4 @@
-function join_by { local IFS="$1"; shift; echo "$*"; }
-function denon {
-    join_by $'\n' "$@"
-    join_by $'\n' "$@" | telnet 'Denon-AVR-X1400H' 23 >/dev/null 2>&1
+function denon() {
+    `dirname $(readlink -f $0)`/denon.py "$@"
 }
 
