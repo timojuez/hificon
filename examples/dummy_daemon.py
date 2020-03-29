5976b286e1318f99a9e2dbbedfa0dd09cb111bfa
Timo <timo.juez@gmail.com>
2020-03-29 17:45:50 +0200

    custom daemon example

---
+++ examples/dummy_daemon.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from Freenon.libdaemon import PluginInterface, EventHandler
+
+
+class MyPlugin(PluginInterface):
+    volume = 50
+    muted = False
+    
+    def getVolume(self):
+        return self.volume
+        
+    def getMuted(self): 
+        return self.muted
+        
+    def update_volume(self, volume):
+        print("[Plugin] volume changed to %d"%volume)
+        self.volume = volume
+        
+    def update_muted(self, muted):
+        print("[Plugin] muted changed to %s"%muted)
+        self.muted = muted
+        
+
+if __name__ == "__main__":
+    plugin = MyPlugin()
+    eh = EventHandler(plugin)
+    print("Current volume: %d"%plugin.volume)
+    while True:
+        newvol = input("Enter new volume: ")
+        plugin.volume = int(newvol)
+        eh.on_plugin_change()
+        
