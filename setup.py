6308f98c30e2f8c79fddadc686f7d77ad095e724
Timo <timo.juez@gmail.com>
2020-04-16 00:25:58 +0200

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='freenon',
-      version='0.4a',
+      version='0.5a',
       description='Network AVR Controlling Software',
       author='Timo Richter',
       author_email='timo.juez@gmail.com',
