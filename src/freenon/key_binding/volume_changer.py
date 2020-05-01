b803642ea83ac6a178b7aa06512fdf799c4b7bc8
Timo <timo.juez@gmail.com>
2020-05-01 14:24:53 +0200

    refactor

---
+++ src/freenon/key_binding/volume_changer.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import time 
+from ..denon import Denon
+from ..config import config
+
+
+BUTTON2CMD = {True:"MVUP", False:"MVDOWN"}
+
+
+class VolumeChanger(object):
+    """ 
+    Class for managing volume up/down while hot key pressed
+    when both hot keys are being pressed, last one counts
+    """
+
+    def __init__(self):
+        self.denon = Denon()
+        self.interval = config.getfloat("KeyEventHandling","interval")/1000
+        self.button = None
+
+    def set_button(self, button):
+        """ set or change currently pressed hot key before or while start() is running """
+        self.button = button
+        
+    def start(self):
+        """ listen for keys and stop when all released """
+        while True:
+            b = self.button
+            if b is None: break
+            self.denon(BUTTON2CMD[b])
+            time.sleep(self.interval)
+
+    def release(self, button):
+        """ button released """
+        if button is not None and self.button != button: return
+        self.stop()
+        
+    def stop(self):
+        self.button = None
+        self.denon.poweron(True)
+        
+        
