0e183e9cee60255e64c94589cd32e967a70786ed
Timo <timo.juez@gmail.com>
2020-05-19 16:47:11 +0200

    update after refactoring

--- examples/custom_app.py
+++ examples/custom_app.py
@@ -1,32 +1,15 @@
 #!/usr/bin/env python3
-from freenon.synctools import PluginInterface, EventHandler
+from freenon.denon import Denon
 
 
-class MyPlugin(PluginInterface):
-    volume = 50
-    muted = False
-    
-    def getVolume(self):
-        return self.volume
-        
-    def getMuted(self): 
-        return self.muted
-        
-    def update_volume(self, volume):
-        print("[Plugin] volume changed to %.1f"%volume)
-        self.volume = volume
-        
-    def update_muted(self, muted):
-        print("[Plugin] muted changed to %s"%muted)
-        self.muted = muted
-        
+def on_avr_change(attr, value):
+    print("Changed %s to %s."%(attr,value))
 
 if __name__ == "__main__":
-    plugin = MyPlugin()
-    eh = EventHandler(plugin)
-    print("Current volume: %.1f"%plugin.volume)
+    denon = Denon(on_avr_change=on_avr_change)
+    denon.connect()
+    print("Current volume: %.1f"%denon.volume)
     while True:
         newvol = input("Enter new volume: ")
-        plugin.volume = float(newvol)
-        eh.on_plugin_change()
+        denon.volume = float(newvol)
         
