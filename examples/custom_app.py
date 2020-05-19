2afd516b805b8e5af652ea61c7f5f170a8f1fc48
Timo <timo.juez@gmail.com>
2020-05-19 16:59:12 +0200

    new:from freenon import Denon; custom_app uses on_connect

--- examples/custom_app.py
+++ examples/custom_app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-from freenon.denon import Denon
+from freenon import Denon
 
 
 def on_avr_change(attr, value):
     print("Changed %s to %s."%(attr,value))
 
-if __name__ == "__main__":
-    denon = Denon(on_avr_change=on_avr_change)
-    denon.connect()
+def volume_prompt():
     print("Current volume: %.1f"%denon.volume)
     while True:
         newvol = input("Enter new volume: ")
         denon.volume = float(newvol)
-        
+
+if __name__ == "__main__":
+    denon = Denon(on_avr_change=on_avr_change, on_connect=volume_prompt)
+    denon.loop()
