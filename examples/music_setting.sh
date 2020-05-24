4329d6ec93b1420fabb7f617b1d648cee99b593a
Timo <timo.juez@gmail.com>
2020-05-24 14:43:07 +0200

    settings

--- examples/music_setting.sh
+++ examples/music_setting.sh
@@ -3,5 +3,15 @@ if [ "$(freenon_cmd PW?)" != "PWON" ]; then
     freenon_cmd PWON
     sleep 3
 fi
-freenon_cmd 'DIM BRI' 'PSFRONT SPB' 'MSSTEREO' 'SISAT/CBL'
+
+# Switch Bass Mode
+if [ "$(freenon_cmd "PSFRONT?")" != "PSFRONT SPB" ]; then
+    freenon_cmd 'MNMEN OFF' 'MNMEN ON' MNCUP MNCUP MNCUP MNCUP MNCUP MNCUP MNCUP MNCDN MNCDN MNCDN MNENT MNCDN MNENT MNCDN MNCDN MNCDN MNCDN MNCDN MNENT MNENT MNCRT MNENT 
+    sleep 2
+    freenon_cmd 'MNMEN OFF'
+fi
+
+freenon_cmd 'DIM BRI' 'PSFRONT SPB' 'SISAT/CBL' 'MSSTEREO' 'PSMULTEQ:OFF'
+sleep 1
+freenon_cmd 'CVSW 50'
 
--- examples/surround_setting.sh
+++ examples/surround_setting.sh
@@ -3,5 +3,15 @@ if [ "$(freenon_cmd PW?)" != "PWON" ]; then
     freenon_cmd PWON
     sleep 3
 fi
-freenon_cmd 'DIM DAR' 'PSFRONT SPA' 'SISAT/CBL' 'MSDTS SURROUND' #'MSDOLBY DIGITAL'
+
+# Switch Bass Mode
+if [ "$(freenon_cmd "PSFRONT?")" != "PSFRONT SPA" ]; then
+    freenon_cmd 'MNMEN OFF' 'MNMEN ON' MNCUP MNCUP MNCUP MNCUP MNCUP MNCUP MNCUP MNCDN MNCDN MNCDN MNENT MNCDN MNENT MNCDN MNCDN MNCDN MNCDN MNCDN MNENT MNENT MNCRT MNENT 
+    sleep 2
+    freenon_cmd 'MNMEN OFF'
+fi
+
+freenon_cmd 'DIM DAR' 'PSFRONT SPA' 'SISAT/CBL' 'MSDTS SURROUND' 'PSMULTEQ:AUDYSSEY' #'MSDOLBY DIGITAL'
+sleep 1
+freenon_cmd 'CVSW 465'
 
