55cd7f9416633f1f452a5f7115c250bb966d48da
Timo <timo.juez@gmail.com>
2020-05-31 14:37:43 +0200

    generalised code

--- src/freenon/key_binding/service.py
+++ src/freenon/key_binding/service.py
@@ -4,16 +4,17 @@
 import selectors, socket, json, sys
 from .volume_changer import VolumeChanger
 
+
 PORT=1234 # TODO
 
 
-class Service(object):
+class JsonService(object):
 
-    def __init__(self):
+    def __init__(self, host="localhost", port=PORT):
         print("[%s] start"%self.__class__.__name__, file=sys.stderr)
         self.sel = selectors.DefaultSelector()
         sock = socket.socket()
-        sock.bind(('localhost', PORT))
+        sock.bind((host, port))
         sock.listen(100)
         sock.setblocking(False)
         self.sel.register(sock, selectors.EVENT_READ, self.accept)
@@ -26,35 +27,32 @@ class Service(object):
                 callback(key.fileobj, mask)
 
     def accept(self, sock, mask):
-        conn, addr = sock.accept()  # Should be ready
-        #print('accepted', conn, 'from', addr)
+        conn, addr = sock.accept()
         conn.setblocking(False)
         self.sel.register(conn, selectors.EVENT_READ, self.read)
 
     def read(self, conn, mask):
-        data = conn.recv(1000)  # Should be ready
+        data = conn.recv(1000)
         if data:
-            #print('read', repr(data))
             try:
                 d = json.loads(data.decode())
-                print("[%s] Received %s"%(self.__class__.__name__,d))
+                print("[%s] Received %s"%(self.__class__.__name__,d), file=sys.stderr)
             except e: print(repr(e))
             else: self.on_read(d)
         else:
-            #print('closing', conn)
             self.sel.unregister(conn)
             conn.close()
 
     def on_read(self, data): pass
     
 
-def send(obj):
+def send(obj, port=PORT):
     sock = socket.socket()
-    sock.connect_ex(("localhost",PORT))
+    sock.connect_ex(("localhost", port))
     sock.send(json.dumps(obj).encode())
 
 
-class VolumeService(Service):
+class VolumeService(JsonService):
 
     def __init__(self):
         print("Key Binding Service")
