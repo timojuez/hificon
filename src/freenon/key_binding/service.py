ddd9472a4441a0743379b977fb9f3ff71a8b03b9
Timo <timo.juez@gmail.com>
2020-06-01 02:24:27 +0200

    moved json_service to util

--- src/freenon/key_binding/service.py
+++ src/freenon/key_binding/service.py
@@ -1,55 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*- 
 
-import selectors, socket, json, sys
+import sys
+from ..util.json_service import JsonService, send
 from .volume_changer import VolumeChanger
-
-
-PORT=1234 # TODO
-
-
-class JsonService(object):
-
-    def __init__(self, host="localhost", port=PORT):
-        print("[%s] start"%self.__class__.__name__, file=sys.stderr)
-        self.sel = selectors.DefaultSelector()
-        sock = socket.socket()
-        sock.bind((host, port))
-        sock.listen(100)
-        sock.setblocking(False)
-        self.sel.register(sock, selectors.EVENT_READ, self.accept)
-
-    def mainloop(self):
-        while True:
-            events = self.sel.select()
-            for key, mask in events:
-                callback = key.data
-                callback(key.fileobj, mask)
-
-    def accept(self, sock, mask):
-        conn, addr = sock.accept()
-        conn.setblocking(False)
-        self.sel.register(conn, selectors.EVENT_READ, self.read)
-
-    def read(self, conn, mask):
-        data = conn.recv(1000)
-        if data:
-            try:
-                d = json.loads(data.decode())
-                print("[%s] Received %s"%(self.__class__.__name__,d), file=sys.stderr)
-            except e: print(repr(e))
-            else: self.on_read(d)
-        else:
-            self.sel.unregister(conn)
-            conn.close()
-
-    def on_read(self, data): pass
-    
-
-def send(obj, port=PORT):
-    sock = socket.socket()
-    sock.connect_ex(("localhost", port))
-    sock.send(json.dumps(obj).encode())
 
 
 class VolumeService(JsonService):
---
+++ src/freenon/util/json_service.py
@@ -0,0 +1,54 @@
+"""
+The JsonService can be used for interprocess communication. It receives dicts.
+The function "send" sends dicts.
+"""
+
+import selectors, socket, json, sys
+
+
+PORT=1234 # TODO
+
+
+class JsonService(object):
+
+    def __init__(self, host="localhost", port=PORT):
+        print("[%s] start"%self.__class__.__name__, file=sys.stderr)
+        self.sel = selectors.DefaultSelector()
+        sock = socket.socket()
+        sock.bind((host, port))
+        sock.listen(100)
+        sock.setblocking(False)
+        self.sel.register(sock, selectors.EVENT_READ, self.accept)
+
+    def mainloop(self):
+        while True:
+            events = self.sel.select()
+            for key, mask in events:
+                callback = key.data
+                callback(key.fileobj, mask)
+
+    def accept(self, sock, mask):
+        conn, addr = sock.accept()
+        conn.setblocking(False)
+        self.sel.register(conn, selectors.EVENT_READ, self.read)
+
+    def read(self, conn, mask):
+        data = conn.recv(1000)
+        if data:
+            try:
+                d = json.loads(data.decode())
+                print("[%s] Received %s"%(self.__class__.__name__,d), file=sys.stderr)
+            except e: print(repr(e))
+            else: self.on_read(d)
+        else:
+            self.sel.unregister(conn)
+            conn.close()
+
+    def on_read(self, data): pass
+    
+
+def send(obj, port=PORT):
+    sock = socket.socket()
+    sock.connect_ex(("localhost", port))
+    sock.send(json.dumps(obj).encode())
+
