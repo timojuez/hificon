10c145d64ccf46d8f30bcb913ef1be2c934e549a
Timo <timo.juez@gmail.com>
2020-06-14 11:13:57 +0200

    renaming to hificon

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 import os
 from distutils.core import setup
 
-setup(name='freenon',
-      version='1.5.2a',
+setup(name='hificon',
+      version='1.5.3a',
       description='Network Amp Controlling Software',
       author='Timo Richter',
       author_email='timo.juez@gmail.com',
       url='',
       license='GNU General Public License v3 or later (GPLv3+)',
-      packages=['freenon'],
-      package_dir={"": "src"},
+      packages=['hificon'],
+      package_dir={"hificon": "src"},
       install_requires=["argparse"],
       extras_require={
         "autosetup": ["python-nmap", "netifaces"],
@@ -20,11 +20,11 @@ setup(name='freenon',
       },
       include_package_data=True,
       entry_points={'console_scripts': [
-        'freenon_cmd = freenon.bin.cmd:main',
-        'freenon_gtk = freenon.bin.gtk:main',
-        'freenon_setup = freenon.bin.setup:main [autosetup]',
-        'freenon_mouse_binding = freenon.bin.mouse_binding:main [nongnu_desktop]',
+        'hifi_sh = hificon.bin.cmd:main',
+        'hificon = hificon.bin.gtk:main',
+        'hificon_setup = hificon.bin.setup:main [autosetup]',
+        'hificon_mouse_binding = hificon.bin.mouse_binding:main [nongnu_desktop]',
       ]},
-      scripts={'src/freenon/bin/freenon_key_event_handler'},
+      scripts={'src/freenon/bin/hificon_key_event_handler'},
 )
 
