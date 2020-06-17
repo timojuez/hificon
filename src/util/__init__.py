d0da347961b5acde95cdf72ccc749c85f0d5ac98
Timo <timo.juez@gmail.com>
2020-06-17 02:36:01 +0200

    log_call only in verbose mode

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -6,7 +6,9 @@ def call_sequence(*functions):
 def log_call(func):
     """ object function decorator """
     def call(self,*args,**xargs):
-        print("[%s] %s"%(self.__class__.__name__, func.__name__), file=sys.stderr)
+        try: assert(self.verbose < 1)
+        except:
+            print("[%s] %s"%(self.__class__.__name__, func.__name__), file=sys.stderr)
         return func(self,*args,**xargs)
     return call
 
