5756d29b16ba6154c7121bef4e75c0da5c850ee4
Timo <timo.juez@gmail.com>
2020-11-06 20:51:58 +0100

    adapted

--- examples/custom_app.py
+++ examples/custom_app.py
@@ -5,30 +5,36 @@ from hificon.amp import features
 from hificon import Amp
 
 
+PROMPT = "Enter new volume: "
+
 def on_feature_change(key, value, prev_value):
-    if not key:
-        print("Received unknown data: %s"%value)
-        return
+    if key:
+        name = amp.features[key].name
 
-    name = amp.features[key].name
-
-    if prev_value is None: # initial call
-        print("Initially setting %s"%name)
-    else:
-        print("Changed %s to %s."%(name, value))
+        if prev_value is None: # initial call
+            print("Initially setting %s"%name)
+        else:
+            print("Changed %s to %s."%(name, value))
+    else: print("Received unknown data: %s"%value)
+    print(PROMPT)
+    
+@features.require("volume") # this function uses amp.volume -> async call
+def print_volume(amp):
+    print("Current volume: %.1f"%amp.volume)
+    print(PROMPT)
 
 @features.require("volume") # this function uses amp.volume -> async call
-def volume_prompt(amp):
-    print("Current volume: %.1f"%amp.volume)
-    while True:
-        newvol = input("Enter new volume: ")
-        if newvol: amp.volume = Decimal(newvol)
+def set_volume(amp, newvol):
+    if newvol: amp.volume = Decimal(newvol)
 
 if __name__ == "__main__":
     amp = Amp()
     amp.bind(on_feature_change=on_feature_change)
     with amp:
         amp.connect()
-        volume_prompt(amp)
-        while True: time.sleep(1)
+        print_volume(amp)
+        while True:
+            print(PROMPT)
+            newvol = input()
+            if newvol: set_volume(amp, newvol)
 
