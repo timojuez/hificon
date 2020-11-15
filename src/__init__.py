fd68c16b00e11ad62482874e0b49c872887813de
Timo <timo.juez@gmail.com>
2020-11-15 17:59:52 +0100

    bugfix: async_widget is thread safe and kivy safe

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ import importlib
 
 NAME = "HiFiCon"
 PKG_NAME = "hificon"
-VERSION = "1.8.31a"
+VERSION = "1.8.32a"
 
 
 def Amp_cls(protocol=None, cls="Amp"):
--- src/util/async_widget.py
+++ src/util/async_widget.py
@@ -15,23 +15,27 @@ def bind_widget_to_value(value_getter, value_setter, widget_getter, widget_sette
         on_value_change(*a) will call get_from_widget(*a)
     """
     lock = Lock()
-    class Previous:
+
+    class Actual:
         is_set = False
         value = None
     
     def on_widget_change(*args):
-        if lock.locked(): return
-        new = widget_getter(*args)
-        if not Previous.is_set:
+        if not Actual.is_set:
             raise RuntimeError("on_value_change must be called initially.")
-        with lock: widget_setter(Previous.value)
-        try: value_setter(new)
+        if lock.locked() and Actual.value == widget_getter(*args): return # Kivy workaround
+        with lock:
+            candidate_value = widget_getter(*args)
+            if candidate_value == Actual.value: return # skip calls caused by widget_setter
+        widget_setter(Actual.value)
+        try: value_setter(candidate_value)
         except Exception as e: print(repr(e))
+
     def on_value_change(*args, **xargs):
         with lock:
+            Actual.value = value_getter()
+            Actual.is_set = True
             widget_setter(value_getter())
-            Previous.value = value_getter()
-            Previous.is_set = True
         
     return on_value_change, on_widget_change
 
