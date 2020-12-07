d39c532b449286ff3b61aafd4340c5468f3c98bb
Timo <timo.juez@gmail.com>
2020-12-07 22:56:30 +0100

    disconnect amp before gtk.quit

--- src/ui/gtk.py
+++ src/ui/gtk.py
@@ -197,7 +197,7 @@ class Icon(Bindable):
         menu.append(item_about)
 
         item_quit = Gtk.MenuItem('Quit')
-        item_quit.connect('activate', lambda *args: Gtk.main_quit())
+        item_quit.connect('activate', lambda *args: (self.amp.exit(), Gtk.main_quit()))
         menu.append(item_quit)
 
         menu.show_all()
