cfe6535118c9d76f0177934d51adda3ab9c0d3b3
Timo <timo.juez@gmail.com>
2020-12-27 04:53:44 +0100

    emulator delays responses; more realistic testing

--- src/protocol/emulator.py
+++ src/protocol/emulator.py
@@ -2,8 +2,9 @@
 Dry software run that acts like a real amp
 """
 
-import math
+import math, time
 from decimal import Decimal
+from threading import Timer, Event
 from ..amp import AbstractAmp, features
 from .. import Amp_cls
 
@@ -27,26 +28,51 @@ def get_val(f):
     return f.encode(val)
 
 
+def mainloop(func):
+    def dec(self, *args, **xargs):
+        self._tasks.append((func, (self,)+args, xargs))
+        self._continue_mainloop.set()
+    return dec
+    
+
 class DummyAmp:
     host = "emulator"
+    _continue_mainloop = Event
+    _tasks = list
     
     def __init__(self, *args, **xargs):
         super().__init__(*args, **xargs)
         self.port = None
+        self._continue_mainloop = self._continue_mainloop()
+        self._tasks = self._tasks()
 
     def connect(self):
+        time.sleep(.05)
         AbstractAmp.connect(self)
         self.on_connect()
 
     def disconnect(self):
         AbstractAmp.disconnect(self)
+        self._continue_mainloop.set()
         self.on_disconnected()
 
-    def mainloop(self):
+    def mainloop_hook(self):
+        AbstractAmp.mainloop_hook(self)
         if not self.connected: self.connect()
+        while self._tasks:
+            func, args, xargs = self._tasks.pop(0)
+            func(*args, **xargs)
+        if self._continue_mainloop.wait(9): self._continue_mainloop.clear()
     
+    @mainloop
     def send(self, cmd):
         AbstractAmp.send(self, cmd)
+        if not self.connected: raise BrokenPipeError("Not connected")
+        time.sleep(.01)
+        Timer(0.05, lambda cmd=cmd: self._receive_dummy_answer(cmd)).start()
+
+    @mainloop
+    def _receive_dummy_answer(self, cmd):
         called_features = [f for key, f in self.features.items() if f.call == cmd]
         
         if called_features:
