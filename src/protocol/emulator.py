0f336ae9474bcc117b626c1ec419622101cf277a
Timo <timo.juez@gmail.com>
2021-01-25 20:53:23 +0100

    moved telnet_emulator into emulator.py

--- src/protocol/emulator.py
+++ src/protocol/emulator.py
@@ -3,7 +3,7 @@ Dry software run that acts like a real amp
 """
 
 from .. import Amp_cls
-from ..core.transport import ProtocolType
+from ..core.transport import ProtocolType, TelnetProtocol
 from ..core.transport.abstract import DummyServerMixin, AbstractClient, AbstractServer
 
 
@@ -40,8 +40,38 @@ class DummyClientMixin:
         self._server.exit()
 
 
-class Amp(ProtocolType):
-    protocol = "Emulator"
+class DummyTelnetClient:
+    """ This client class automatically connects to a dummy server instance """
+    _server = None
+
+    def enter(self):
+        self._server.enter()
+        super().enter()
+    
+    def exit(self):
+        super().exit()
+        self._server.exit()
+
+
+class TelnetEmulator(ProtocolType):
+    protocol = "Telnet Emulator"
+
+    @classmethod
+    def new_client(cls, protocol, port=0, *args, **xargs):
+        print(protocol)
+        Protocol = Amp_cls(protocol)
+        server = Protocol.new_dummyserver(listen_host="127.0.0.1", listen_port=int(port))
+        xargs.update(host=server.host, port=server.port)
+        client = type(Protocol.__name__, (DummyTelnetClient, Protocol, Protocol.Client), {})(*args, **xargs)
+        client._server = server
+        return client
+    
+    @classmethod
+    def new_server(*args, **xargs): raise NotImplementedError()
+
+
+class PlainEmulator(ProtocolType):
+    protocol = "Plain Emulator"
     
     @classmethod
     def new_client(cls, protocol, *args, **xargs):
@@ -53,6 +83,19 @@ class Amp(ProtocolType):
         client.bind(send = lambda data: server.on_receive_raw_data(data))
         return client
     
+    @classmethod
+    def new_server(*args, **xargs): raise NotImplementedError()
+
+
+class Amp(ProtocolType):
+    protocol = "Emulator"
+
+    @classmethod
+    def new_client(cls, protocol, *args, **xargs):
+        Protocol = Amp_cls(protocol)
+        Emulator = TelnetEmulator if issubclass(Protocol, TelnetProtocol) else PlainEmulator
+        return Emulator.new_client(protocol, *args, **xargs)
+
     @classmethod
     def new_server(*args, **xargs): raise NotImplementedError()
 
