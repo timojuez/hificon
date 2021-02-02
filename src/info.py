84bc4b23354a62bf6ecfdf50d313031c4e9d53df
Timo <timo.juez@gmail.com>
2021-02-02 01:45:59 +0100

    own icons

--- src/info.py
+++ src/info.py
@@ -3,6 +3,5 @@ PKG_NAME = "hificon"
 VERSION = "1.10.0a"
 AUTHOR = "Timo L. Richter"
 URL = 'https://github.com/timojuez/hificon'
-COPYRIGHT = ("Copyright \xa9 2021 %s\n"
-    "Icons based on Ubuntu Mono Dark (GNU GPLv3)")%AUTHOR
+COPYRIGHT = f"Copyright \xa9 2021 {AUTHOR}\n"
 
