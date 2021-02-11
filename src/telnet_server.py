cd3de0cec17c953e7caa77e0b2fee7fe6495acd9
Timo <timo.juez@gmail.com>
2021-02-11 23:43:11 +0100

    ClientRepeater is abstract and not telnet specific

--- src/telnet_server.py
+++ src/telnet_server.py
@@ -1,15 +1,14 @@
-import argparse, sys
-from .core import AbstractServer, AbstractClient, TelnetServer
+import argparse
 from . import Target
 
 
-class ClientRepeater(TelnetServer):
+class ClientRepeaterMixin:
     client = None
     
-    def __init__(self, listen_host, listen_port, linebreak, *args, **xargs):
-        self.client = Target(*args, role="client", **xargs)
+    def __init__(self, target, *args, **xargs):
+        self.client = target
         self.client.bind(on_receive_raw_data = lambda data:self.send(data))
-        super().__init__(listen_host, listen_port, linebreak)
+        super().__init__(*args, **xargs)
     
     def enter(self):
         super().enter()
@@ -25,6 +24,11 @@ class ClientRepeater(TelnetServer):
     def on_receive_raw_data(self, data): self.client.send(data)
 
 
+def create_client_repeater(*args, kwargs_server={}, **xargs):
+    target = Target(*args, **xargs)
+    return type("Server", (ClientRepeaterMixin, target.Server), {})(target, **kwargs_server)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Start a server for interacting on a given protocol')
     parser.add_argument('--listen-host', metavar="HOST", type=str, default="127.0.0.1", help='Host (listening)')
@@ -38,9 +42,9 @@ def main():
     parser.add_argument('-n', '--newline', action="store_const", default="\r", const="\n", help='Print \\n after each line (not native bahaviour)')
     parser.add_argument('--verbose', '-v', action='count', default=0, help='Verbose mode')
     args = parser.parse_args()
-    xargs = dict(uri=args.target, listen_host=args.listen_host, listen_port=args.listen_port, linebreak=args.newline, verbose=args.verbose+1)
-    if args.repeat: server = ClientRepeater(**xargs)
-    else: server = Target(role=args.role, **xargs)
+    xargs = dict(listen_host=args.listen_host, listen_port=args.listen_port, linebreak=args.newline, verbose=args.verbose+1)
+    if args.repeat: server = create_client_repeater(uri=args.target, kwargs_server=xargs)
+    else: server = Target(uri=args.target, role=args.role, **xargs)
     with server:
         while True: server.send(input())
 
