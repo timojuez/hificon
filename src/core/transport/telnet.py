31f08d7c4c68e4b0da7fbc54275fa6c9652e4927
Timo <timo.juez@gmail.com>
2021-03-06 17:30:52 +0100

    telnet server verbosity bugfix

--- src/core/transport/telnet.py
+++ src/core/transport/telnet.py
@@ -149,7 +149,7 @@ class TelnetServer(AbstractServer):
     _server = None
     
     def __init__(self, listen_host="127.0.0.1", listen_port=0, linebreak="\r", *args, verbose=0, **xargs):
-        super().__init__(*args, verbose=min(0, verbose-1), **xargs)
+        super().__init__(*args, verbose=max(0, verbose-1), **xargs)
         if listen_host.startswith("//"): listen_host = listen_host[2:]
         self._server = _TelnetServer(self, listen_host, int(listen_port), linebreak, verbose=verbose)
     
