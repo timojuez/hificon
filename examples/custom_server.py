6659f143e265defa85dd43beafe7679e8b1a432b
Timo <timo.juez@gmail.com>
2021-04-25 00:19:46 +0200

    server example: external_counter moved to CounterFeature

--- examples/custom_server.py
+++ examples/custom_server.py
@@ -1,53 +1,50 @@
 """
-This shows how to share values from ExternalEntity with clients in a client-server architecture.
+This shows how to share values from an external entity with clients in a client-server architecture.
 
 Run server:
-    examples$ python3 -m hificon.server -t custom_server.MyScheme://127.0.0.1:1234:900
+    examples$ python3 -m hificon.server -t custom_server.MyScheme://127.0.0.1:1234:40
 Run client:
     examples$ python3 -m hificon.hifish -t custom_server.MyScheme://127.0.0.1:1234
     The value $counter shall always be synchronised with the server.
 """
 
 import time
-from threading import Thread
+from threading import Thread, Lock
 from hificon.core.transport.telnet import TelnetServer, TelnetScheme
 from hificon.amp import TelnetAmp
 from hificon.core.transport import features
 
 
-class ExternalEntity:
-    """ This just increases numbers independently. Let's assume this is only possible on the server. """
+class ExternalCounter:
+    """ This just increases numbers independently. This is only being run on the server. """
     value = 0
+    lock = Lock()
 
-    def __init__(self, target, start):
-        self.target = target
-        self.value = start
+    def __init__(self, feature):
+        self.feature = feature
         Thread(target=self.count, daemon=True).start()
 
     def count(self):
         while True:
-            self.value = (self.value+1)%50
-            self.target.counter = self.value # sync clients
+            self.set((self.value+1)%50)
             time.sleep(1)
 
     def set(self, value):
-        """ call caused by client """
         if value < 50:
-            self.value = value
-            self.target.counter = self.value # sync clients
+            with self.lock:
+                self.value = value
+                self.feature.set(self.value) # sync clients
 
 
 class MyServer(TelnetServer):
     """ Adds a custom value START_COUNT to init and changes the URI to
     SCHEME://IP:PORT:START_COUNT
-    init initiates the connection to ExternalEntity
     """
     init_args_help = ("//LISTEN_IP", "LISTEN_PORT", "START_COUNT")
-    external_entity = None
     
-    def __init__(self, ip="127.0.0.1", port=0, start=0, *args, **xargs):
+    def __init__(self, ip="127.0.0.1", port=0, start=None, *args, **xargs):
         super().__init__(ip, port, *args, **xargs)
-        self.external_entity = ExternalEntity(self, int(start))
+        if start: self.features.counter.external_counter.set(int(start))
 
 
 class MyScheme(TelnetScheme):
@@ -58,7 +55,9 @@ class MyScheme(TelnetScheme):
 
 
 @MyScheme.add_feature
-class Counter(features.IntFeature):
+class CounterFeature(features.IntFeature):
+    """ connects ExternalCounter with the server and clients """
+    key = "counter"
     min = 0
     max = 50
     call = "get counter"
@@ -66,6 +65,7 @@ class Counter(features.IntFeature):
     def serialize(self, value): return "set counter %d"%value
     def unserialize(self, data): return int(data.rsplit(" ",1)[1])
     def matches(self, data): return data.startswith("set counter")
-    def set_on_server(self, value): self.target.external_entity.set(value)
-    def poll_on_server(self): self.set(self.target.external_entity.value)
+    def init_on_server(self): self.external_counter = ExternalCounter(self)
+    def set_on_server(self, value): self.external_counter.set(value)
+    def poll_on_server(self): self.set(self.external_counter.value)
 
