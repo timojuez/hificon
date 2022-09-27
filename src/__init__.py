c143934712d921b2052022c86619c7961d8bbe10
Timo <timo.juez@gmail.com>
2022-09-27 23:43:19 +0200

    moved get_scheme and get_schemes from src/__init__ to src/schemes/__init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,26 +1,6 @@
-import importlib
 from .core import config, AbstractScheme, AbstractServer, AbstractClient, features
 from .info import *
-from .schemes import schemes
-
-
-def get_schemes():
-    for p in schemes.keys(): yield get_scheme(p)
-
-
-def get_scheme(cls_path):
-    """ @cls_path str: Key in .schemes.schemes or "module.class" """
-    try: cls_path_ = schemes[cls_path]
-    except KeyError: cls_path_ = cls_path
-    if "." not in cls_path_:
-        schemes_str = ", ".join(schemes.keys())
-        raise ValueError(f"cls_path must be {schemes_str} or MODULE.CLASS but was '{cls_path}'.")
-    module_path, cls = cls_path_.rsplit(".", 1)
-    module = importlib.import_module(module_path, "%s.schemes"%__name__)
-    Scheme = getattr(module, cls)
-    Scheme.scheme = cls_path
-    assert(issubclass(Scheme, AbstractScheme))
-    return Scheme
+from .schemes import schemes, get_scheme, get_schemes
 
 
 def Target(uri=None, *args, role="client", **xargs):
--- src/schemes/__init__.py
+++ src/schemes/__init__.py
@@ -1,3 +1,6 @@
+import importlib
+from ..core import AbstractScheme
+
 schemes = {
     "denon": ".denon.Denon",
     "emulate": ".emulate.Emulate",
@@ -6,4 +9,23 @@ schemes = {
     "auto": ".auto.Auto",
     "repeat": ".repeat.Repeat"
 }
+
+
+def get_schemes():
+    for p in schemes.keys(): yield get_scheme(p)
+
+
+def get_scheme(cls_path):
+    """ @cls_path str: Key in .schemes.schemes or "module.class" """
+    try: cls_path_ = schemes[cls_path]
+    except KeyError: cls_path_ = cls_path
+    if "." not in cls_path_:
+        schemes_str = ", ".join(schemes.keys())
+        raise ValueError(f"cls_path must be {schemes_str} or MODULE.CLASS but was '{cls_path}'.")
+    module_path, cls = cls_path_.rsplit(".", 1)
+    module = importlib.import_module(module_path, __name__)
+    Scheme = getattr(module, cls)
+    Scheme.scheme = cls_path
+    assert(issubclass(Scheme, AbstractScheme))
+    return Scheme
 
