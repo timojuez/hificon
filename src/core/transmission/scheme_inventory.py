004990e9b18920d91559b24ad742f6a339842f6a
Timo <timo.juez@gmail.com>
2022-10-12 23:02:11 +0200

    allow Scheme classes in scheme_inventory.schemes

--- src/core/transmission/scheme_inventory.py
+++ src/core/transmission/scheme_inventory.py
@@ -12,20 +12,22 @@ schemes = {
 
 
 def get_schemes():
-    for p in schemes.keys(): yield get_scheme(p)
+    for key in schemes.keys(): yield get_scheme(key)
 
 
 def get_scheme(cls_path):
     """ @cls_path str: Key in .schemes.schemes or "module.class" """
-    try: cls_path_ = schemes[cls_path]
-    except KeyError: cls_path_ = cls_path
-    if "." not in cls_path_:
-        schemes_str = ", ".join(schemes.keys())
-        raise ValueError(f"cls_path must be {schemes_str} or MODULE.CLASS but was '{cls_path}'.")
-    module_path, cls = cls_path_.rsplit(".", 1)
-    root_module = __package__.rsplit(".", 2)[0]
-    module = importlib.import_module(module_path, f"{root_module}.schemes")
-    Scheme = getattr(module, cls)
+    try: cls_or_path_ = schemes[cls_path]
+    except KeyError: cls_or_path_ = cls_path
+    if isinstance(cls_or_path_, str):
+        if "." not in cls_or_path_:
+            schemes_str = ", ".join(schemes.keys())
+            raise ValueError(f"cls_path must be {schemes_str} or MODULE.CLASS but was '{cls_path}'.")
+        module_path, cls = cls_or_path_.rsplit(".", 1)
+        root_module = __package__.rsplit(".", 2)[0]
+        module = importlib.import_module(module_path, f"{root_module}.schemes")
+        Scheme = getattr(module, cls)
+    else: Scheme = cls_or_path_
     Scheme.scheme = cls_path
     Scheme.Scheme = Scheme
     assert(issubclass(Scheme, SchemeType))
