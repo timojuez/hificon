a4ccea265b8c6c22daf5af80a9cea9e6f11164c0
Timo <timo.juez@gmail.com>
2022-10-17 21:17:15 +0200

    Autostart class takes new parameters: name, package, terminal

--- src/tray/setup.py
+++ src/tray/setup.py
@@ -8,9 +8,17 @@ from .. import NAME, PKG_NAME, Target
 class _Autostart:
     path = None
 
+    def __init__(self, name, module=__package__, terminal=False):
+        self.name = name
+        self.module = module
+        self.terminal = terminal
+
     def get_active(self): return os.path.exists(self.path)
+
     def set_active(self, value): self.activate() if value else self.deactivate()
+
     def activate(self): raise NotImplementedError()
+
     def deactivate(self):
         try: os.remove(self.path)
         except FileNotFoundError: pass
@@ -23,23 +31,35 @@ class AutostartWin(_Autostart):
         import getpass
         user = getpass.getuser()
         self.path = (f"C:\\Users\\{user}\\AppData\\Roaming\\Microsoft\\Windows"
-            f"\\Start Menu\\Programs\\Startup\\{PKG_NAME}.bat")
+            f"\\Start Menu\\Programs\\Startup\\{self.name}.bat")
 
     def activate(self):
+        cmd = "python.exe -m {self.module}" if self.terminal \
+            else "pythonw.exe -m {self.module} 1>NUL 2>&1"
         with open(self.path, "w") as fp:
-            fp.write(f'start "" "pythonw.exe -m {__package__} 1>NUL 2>&1"')
+            fp.write(f'start "" "{cmd}"')
 
 
 class AutostartGnu(_Autostart):
 
     def __init__(self, *args, **xargs):
         super().__init__(*args, **xargs)
-        self.path = os.path.expanduser(f"~/.config/autostart/{PKG_NAME}.desktop")
+        self.path = os.path.expanduser(f"~/.config/autostart/{self.name}.desktop")
 
     def activate(self):
-        desktop = pkgutil.get_data(__name__,"../share/hificon_tray.desktop").decode()
-        with open(os.path.expanduser(self.path, "w") as fp:
-            fp.write(desktop)
+        content = "\n".join([
+            "[Desktop Entry]",
+            "Encoding=UTF-8",
+            "Type=Application",
+            f"Name={self.name}",
+            f"Exec=/usr/bin/env python3 -m {self.module}",
+            "StartupNotify=false",
+            f"Terminal={str(self.terminal).lower()}",
+            "Hidden=false",
+            "X-GNOME-Autostart-enabled=true",
+        ])
+        with open(self.path, "w") as fp:
+            fp.write(content)
 
 
 Autostart = AutostartWin if sys.platform.startswith("win") else AutostartGnu
@@ -47,7 +67,7 @@ Autostart = AutostartWin if sys.platform.startswith("win") else AutostartGnu
 
 def autostart():
     if input("Add %s to autostart for current user? [Y/n] "%NAME) == "n": return
-    Autostart().activate()
+    Autostart(PKG_NAME, __package__, terminal=False).activate()
 
 
 def source_setup():
