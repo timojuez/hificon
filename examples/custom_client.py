1aed1df05d60676c920b7a8bbfd72df6aee4cff6
Timo <timo.juez@gmail.com>
2022-11-03 22:58:57 +0100

    example update

--- examples/custom_client.py
+++ examples/custom_client.py
@@ -7,24 +7,23 @@ from hificon import Target
 PROMPT = "Enter new volume: "
 
 
-def on_feature_change(key, value):
-    name = target.features[key].name
-    print("Changed %s to %s."%(name, value))
+def on_receive_feature_value(f, value):
+    print("Changed %s to %s."%(f.name, value))
     print(PROMPT)
-    
 
-def print_volume():
-    print("Current volume: %.1f"%target.features.volume.get())
+
+def print_volume(volume):
+    print("Current volume: %.1f"%volume.get())
     print(PROMPT)
 
 
 if __name__ == "__main__":
     #target = Target()
     ## for testing:
-    target = Target("emulator:denon")
+    target = Target("emulate:denon")
 
     target.features.volume.bind(on_set = lambda: print("Initially setting volume"))
-    target.bind(on_feature_change = on_feature_change)
+    target.bind(on_receive_feature_value = on_receive_feature_value)
     with target:
         target.connect()
         target.schedule(print_volume, requires=("volume",)) # this function needs target.volume -> schedule call
