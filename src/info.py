d641e7785f3f56a971d3b5ea91f0bb8ffac8b048
Timo <timo.juez@gmail.com>
2023-01-02 21:30:19 +0100

    copyright year

--- src/info.py
+++ src/info.py
@@ -3,5 +3,5 @@ PKG_NAME = "hificon"
 VERSION = "2.0.0"
 AUTHOR = "Timo L. Richter"
 URL = 'https://github.com/timojuez/hificon'
-COPYRIGHT = f"Copyright \xa9 2022 {AUTHOR}\n"
+COPYRIGHT = f"Copyright \xa9 2022-2023 {AUTHOR}\n"
 
